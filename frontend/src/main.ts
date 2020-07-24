f930296821d5a8a088c7fdb99cbb94bf1b7f4c26
Riccardo Cavallin <riccardocavallin14@gmail.com>
2020-07-24 12:28:49 +0200

    fix package json

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import Buefy from 'buefy';
-import VueResource from 'vue-resource'
-import 'buefy/dist/buefy.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Buefy from "buefy";
+import VueResource from "vue-resource";
+import "buefy/dist/buefy.css";
 
 Vue.config.productionTip = false;
-
 
 Vue.use(Buefy);
 Vue.use(VueResource);
@@ -16,4 +15,4 @@ new Vue({
   router,
   store,
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount("#app");
